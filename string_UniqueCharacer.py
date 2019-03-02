6ff8736c2232e6504f21b581f309500db480ac63
qingyueyang <36853007+qingyueyang@users.noreply.github.com>
2019-03-01 23:22:11 -0800

    string

--- string_UniqueCharacer.py
+++ string_UniqueCharacer.py
@@ -1,39 +1,16 @@
-def is_unique_bit(string):
-  if len(string) > 128:
+def isUnique(str):
+  if len(str) > 256:
     return False
 
-  unique_tracker = 0
-  for char in string:
-    ascii_val = ord(char)
-    if (unique_tracker & (1 << ascii_val)) > 0:
+  tracker = 0
+  for char in str:
+    val = ord(char)
+    if (tracker & (1 << val)) > 0:
       return False
-    unique_tracker |= (1 << ascii_val)
+    tracker |= (1 << val)
   return True
 
-def isUniqueChars(st): 
-  
-    # String length cannot be more than 
-    # 256. 
-    if len(st) > 256: 
-        return False
-  
-    # Initialize occurrences of all characters 
-    char_set = [False] * 128
-  
-    # For every character, check if it exists 
-    # in char_set 
-    for i in range(0, len(st)): 
-  
-        # Find ASCII value and check if it 
-        # exists in set. 
-        val = ord(st[i]) 
-        if char_set[val]: 
-            return False
-  
-        char_set[val] = True
-  
-    return True
 
 #test
-string = "12god"
-print(is_unique_bit(string))
+string = "122gGod"
+print(isUnique(string))
--- string_Urlify.py
+++ string_Urlify.py
@@ -1,10 +1,11 @@
-def spaceReplace(str):
-    strArr = list(str)
-    for i, c in enumerate(strArr):
-        if c == ' ':
-            strArr[i] = '%20'
-    
-    return ''.join(strArr)
+def spaceReplace(s):
+    str = ""
+    for x in s:
+        if x == " ":
+            str += "%20"
+        else:
+            str += x
+    return str
 
 str = 'winter is coming'
 print(spaceReplace(str))
